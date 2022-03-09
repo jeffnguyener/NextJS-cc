0005448d7e0abd8029b78180a05715c738462f6f
hjnguyen <hnguyen@health.southalabama.edu>
2022-03-09 15:31:32 -0600

    pages & route setup

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+
+const about = () => {
+    return (
+        <div>
+            <title>About</title>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about
